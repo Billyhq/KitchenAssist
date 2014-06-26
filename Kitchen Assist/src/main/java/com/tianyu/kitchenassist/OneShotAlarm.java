b1158d99725e9cbc534bf0b0917f5ab1a7bdcd8d
Billyhq <billyhq@gmail.com>
2014-06-26 12:57:27 +0800

    Update OneShotAlarm.java
    add notification when OneShotAlarm receives alarm broadcast

--- Kitchen Assist/src/main/java/com/tianyu/kitchenassist/OneShotAlarm.java
+++ Kitchen Assist/src/main/java/com/tianyu/kitchenassist/OneShotAlarm.java
@@ -15,11 +15,13 @@ package com.tianyu.kitchenassist;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import android.app.NotificationManager;
+import android.app.Notification;
+import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
 import android.content.BroadcastReceiver;
 import android.widget.Toast;
-
 // Need the following import to get access to the app resources, since this
 // class is in a sub-package.
 
@@ -35,6 +37,20 @@ public class OneShotAlarm extends BroadcastReceiver
     public void onReceive(Context context, Intent intent)
     {
         Toast.makeText(context, "message received", Toast.LENGTH_SHORT).show();
+        Notification.Builder builder = new Notification.Builder(context)
+                .setTicker("主人，有食品要过期了")
+                .setSmallIcon(android.R.drawable.stat_notify_chat)
+                .setContentTitle("厨房助手")
+                .setContentText("打开瞅瞅")
+                .setAutoCancel(true)
+                .setContentIntent(TaskStackBuilder.create(context)
+                        .addParentStack(ListViewActivity.class)
+                        .addNextIntent(new Intent(context, ListViewActivity.class)
+                                .putExtra("test", "From Notification"))
+                        .getPendingIntent(0, 0));
+        builder.setDefaults(builder.build().DEFAULT_ALL);
+        NotificationManager nm = (NotificationManager)context.getSystemService(context.NOTIFICATION_SERVICE);
+        nm.notify("direct_tag", R.string.notification, builder.build());
         intent = new Intent(this, ListViewActivity.class);
     }
 }
